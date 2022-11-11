acb2934298e015c95172809fa77e8ab45df3577b
Hindax <haar162@more2learn.de>
2022-11-11 09:29:04 +0100

    test

--- script_01.js
+++ script_01.js
@@ -2,3 +2,21 @@
 
 /***** Funktionen 01 *****/
 
+// 1. Kapselung von Codeblöcken
+
+//Funktionsrumpf (body) callee
+
+
+function test()
+{
+    console.log("Hallo Sabine!");
+}
+
+
+// Funktionsaufruf (call)
+test();
+test();
+test();
+test();
+
+
